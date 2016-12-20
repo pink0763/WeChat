9b2400910e22b047061cf514f0a231cbf8a33911
pink0763 <pengchen0719@hotmail.com>
2016-12-20 09:31:28 +0800

    Create Master

---
+++ src/com/innovaee/test/WeixinTest.java
@@ -0,0 +1,51 @@
+package com.innovaee.test;
+
+import com.innovaee.po.AccessToken;
+import com.innovaee.util.MessageUtil;
+import com.innovaee.util.WeixinUtil;
+
+import net.sf.json.JSONObject;
+
+public class WeixinTest {
+	public static void main(String[] args) {
+		try {
+			AccessToken token = WeixinUtil.getAccessToken();
+				
+//			MusicMessage musicMessage = JSONObject.fromObject(MessageUtil.initMusicMessage(toUserName, fromUserName));
+//			int result = MessageUtil.musicMessageToXml(musicMessage);
+			
+			//创建音乐消息
+//			System.out.println("票据"+token.getToken());
+//			System.out.println("有效时间"+token.getExpiresIn());
+//			
+//			String path = "D:/1.png";
+//			String mediaId = WeixinUtil.upload(path, token.getToken(), "thumb");
+//			System.out.println(mediaId);
+			
+			//创建图片消息
+//			System.out.println("票据"+token.getToken());
+//			System.out.println("有效时间"+token.getExpiresIn());
+//			
+//			String path = "D:/Windows.png";
+//			String mediaId = WeixinUtil.upload(path, token.getToken(), "image");
+//			System.out.println(mediaId);
+			
+			//创建菜单
+			String menu = JSONObject.fromObject(WeixinUtil.initMenu()).toString();
+			int result1 = WeixinUtil.deleteMenu(token.getToken());
+			if(result1==0) {
+				System.out.println("删除菜单成功");
+			}else {
+				System.out.println("错误码："+result1);
+			}
+			int result = WeixinUtil.createMenu(token.getToken(), menu);
+			if(result==0) {
+				System.out.println("创建菜单成功");
+			}else {
+				System.out.println("错误码："+result);
+			}			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/com/innovaee/util/MessageUtil.java
@@ -0,0 +1,241 @@
+package com.innovaee.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+import com.innovaee.po.Image;
+import com.innovaee.po.ImageMessage;
+import com.innovaee.po.Music;
+import com.innovaee.po.MusicMessage;
+import com.innovaee.po.News;
+import com.innovaee.po.NewsMessage;
+import com.innovaee.po.TextMessage;
+import com.thoughtworks.xstream.XStream;
+/**
+ * 消息封装类
+ * @author Stephen
+ *
+ */
+public class MessageUtil {	
+	
+	public static final String MESSAGE_TEXT = "text";
+	public static final String MESSAGE_NEWS = "news";
+	public static final String MESSAGE_IMAGE = "image";
+	public static final String MESSAGE_VOICE = "voice";
+	public static final String MESSAGE_MUSIC = "music";
+	public static final String MESSAGE_VIDEO = "video";
+	public static final String MESSAGE_LINK = "link";
+	public static final String MESSAGE_LOCATION = "location";
+	public static final String MESSAGE_EVNET = "event";
+	public static final String MESSAGE_SUBSCRIBE = "subscribe";
+	public static final String MESSAGE_UNSUBSCRIBE = "unsubscribe";
+	public static final String MESSAGE_CLICK = "CLICK";
+	public static final String MESSAGE_VIEW = "VIEW";
+	public static final String MESSAGE_SCANCODE= "scancode_push";
+	
+	/**
+	 * xml转为map集合
+	 * @param request
+	 * @return
+	 * @throws IOException
+	 * @throws DocumentException
+	 */
+	public static Map<String, String> xmlToMap(HttpServletRequest request) throws IOException, DocumentException{
+		Map<String, String> map = new HashMap<String, String>();
+		SAXReader reader = new SAXReader();
+		
+		InputStream ins = request.getInputStream();
+		Document doc = reader.read(ins);
+		
+		Element root = doc.getRootElement();
+		
+		List<Element> list = root.elements();
+		
+		for(Element e : list){
+			map.put(e.getName(), e.getText());
+		}
+		ins.close();
+		return map;
+	}
+	
+	/**
+	 * 将文本消息对象转为xml
+	 * @param textMessage
+	 * @return
+	 */
+	public static String textMessageToXml(TextMessage textMessage){
+		XStream xstream = new XStream();
+		xstream.alias("xml", textMessage.getClass());
+		return xstream.toXML(textMessage);
+	}
+	
+	/**
+	 * 组装文本消息
+	 * @param toUserName
+	 * @param fromUserName
+	 * @param content
+	 * @return
+	 */
+	public static String initText(String toUserName,String fromUserName,String content){
+		TextMessage text = new TextMessage();
+		text.setFromUserName(toUserName);
+		text.setToUserName(fromUserName);
+		text.setMsgType(MessageUtil.MESSAGE_TEXT);
+		text.setCreateTime(new Date().getTime());
+		text.setContent(content);
+		return textMessageToXml(text);
+	}
+	
+	/**
+	 * 主菜单
+	 * @return
+	 */
+	public static String menuText(){
+		StringBuffer sb = new StringBuffer();
+		sb.append("欢迎您的关注，请按照菜单提示进行操作：\n\n");
+		sb.append("1、课程介绍\n");
+		sb.append("2、培训认证\n");
+		sb.append("3、联系我们\n\n");
+		sb.append("回复？调出此菜单。");
+		return sb.toString();
+	}
+	
+	public static String firstMenu(){
+		StringBuffer sb = new StringBuffer();
+		sb.append("课程内容包括Java EE开发、H5开发、iOS开发等");
+		return sb.toString();
+	}
+	
+	public static String secondMenu(){
+		StringBuffer sb = new StringBuffer();
+		sb.append("功能包括讲师介绍和培训证书查询");
+		return sb.toString();
+	}
+	
+	public static String thirdMenu(){
+		StringBuffer sb = new StringBuffer();
+		sb.append("功能包括学员问题反馈和课程报名");
+		return sb.toString();
+	}
+	/**
+	 * 图文消息转为xml
+	 * @param newsMessage
+	 * @return
+	 */
+	public static String newsMessageToXml(NewsMessage newsMessage){
+		XStream xstream = new XStream();
+		xstream.alias("xml", newsMessage.getClass());
+		xstream.alias("item", new News().getClass());
+		return xstream.toXML(newsMessage);
+	}
+	
+	/**
+	 * 图片消息转为xml
+	 * @param imageMessage
+	 * @return
+	 */
+	public static String imageMessageToXml(ImageMessage imageMessage){
+		XStream xstream = new XStream();
+		xstream.alias("xml", imageMessage.getClass());
+		return xstream.toXML(imageMessage);
+	}
+	
+	/**
+	 * 音乐消息转为xml
+	 * @param musicMessage
+	 * @return
+	 */
+	public static String musicMessageToXml(MusicMessage musicMessage){
+		XStream xstream = new XStream();
+		xstream.alias("xml", musicMessage.getClass());
+		return xstream.toXML(musicMessage);
+	}
+	/**
+	 * 图文消息的组装
+	 * @param toUserName
+	 * @param fromUserName
+	 * @return
+	 */
+	public static String initNewsMessage(String toUserName,String fromUserName){
+		String message = null;
+		List<News> newsList = new ArrayList<News>();
+		NewsMessage newsMessage = new NewsMessage();
+		
+		News news = new News();
+		news.setTitle("培训认证");
+		news.setDescription("了解教授课程的讲师和培训证书信息的查询");
+		//设置标题图片地址链接
+		news.setPicUrl("http://475a497c.ngrok.io/Weixin/image/title2.jpg");
+		news.setUrl("www.innovaee.com");
+		
+		newsList.add(news);
+		
+		newsMessage.setToUserName(fromUserName);
+		newsMessage.setFromUserName(toUserName);
+		newsMessage.setCreateTime(new Date().getTime());
+		newsMessage.setMsgType(MESSAGE_NEWS);
+		newsMessage.setArticles(newsList);
+		newsMessage.setArticleCount(newsList.size());
+		
+		message = newsMessageToXml(newsMessage);
+		return message;
+	}
+	
+	/**
+	 * 组装图片消息
+	 * @param toUserName
+	 * @param fromUserName
+	 * @return
+	 */
+	public static String initImageMessage(String toUserName,String fromUserName){
+		String message = null;
+		Image image = new Image();
+		//通过新增临时素材或永久素材接口，获得上传媒体文件的id
+		image.setMediaId("1G9IY7GxhlkWUOO1FVqUDXSZVUKoF8X_JYWHLPg1EQPyEJFMVuELz8wzbJPFzND8");
+		ImageMessage imageMessage = new ImageMessage();
+		imageMessage.setFromUserName(toUserName);
+		imageMessage.setToUserName(fromUserName);
+		imageMessage.setMsgType(MESSAGE_IMAGE);
+		imageMessage.setCreateTime(new Date().getTime());
+		imageMessage.setImage(image);
+		message = imageMessageToXml(imageMessage);
+		return message;
+	}
+	
+	/**
+	 * 组装音乐消息
+	 * @param toUserName
+	 * @param fromUserName
+	 * @return
+	 */
+	public static String initMusicMessage(String toUserName,String fromUserName){
+		String message = null;
+		Music music = new Music();
+		music.setThumbMediaId("NfUxtkqGN920M7mvOi2X4_JW8-0Sd_xHjbCKvOAAR5zZuPur3vDfGwViQ2ost1Wr");
+		music.setTitle("see you again");
+		music.setDescription("速7片尾曲");
+		music.setMusicUrl("http://475a497c.ngrok.io/Weixin/resource/See You Again.mp3");
+		music.setHQMusicUrl("http://475a497c.ngrok.io/Weixin/resource/See You Again.mp3");
+		
+		MusicMessage musicMessage = new MusicMessage();
+		musicMessage.setFromUserName(toUserName);
+		musicMessage.setToUserName(fromUserName);
+		musicMessage.setMsgType(MESSAGE_MUSIC);
+		musicMessage.setCreateTime(new Date().getTime());
+		musicMessage.setMusic(music);
+		message = musicMessageToXml(musicMessage);
+		return message;
+	}
+}
